ba6e2e61ab5905bb3c7fcdc499707a6efa32ff33
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-03-05 00:01:25 +0700

    add google map api

--- IOS/TripsTap/TripsTap/AppDelegate.swift
+++ IOS/TripsTap/TripsTap/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyB2KaJa01bnVnRy__1GUK17ownU-ZeuMYQ");
         return true
     }
 
