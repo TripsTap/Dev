1023757770acb70a80af62717588caa58ff7286f
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-03-07 15:24:07 +0700

    created  main . map , plan information page

--- IOS/TripsTap/TripsTap/ViewController.swift
+++ IOS/TripsTap/TripsTap/ViewController.swift
@@ -11,23 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        var camera = GMSCameraPosition.cameraWithLatitude(0, longitude:-165, zoom:2)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
-        
-        var path = GMSMutablePath()
-        path.addLatitude(-33.866, longitude:151.195) // Sydney
-        path.addLatitude(-18.142, longitude:178.431) // Fiji
-        path.addLatitude(21.291, longitude:-157.821) // Hawaii
-        path.addLatitude(37.423, longitude:-122.091) // Mountain View
-        
-        var polyline = GMSPolyline(path: path)
-        polyline.strokeColor = UIColor.blueColor()
-        polyline.strokeWidth = 1.0
-        polyline.map = mapView
-        
-        self.view = mapView
+//        super.viewDidLoad()
+//        
+//        var camera = GMSCameraPosition.cameraWithLatitude(0, longitude:-165, zoom:2)
+//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+//        
+//        var path = GMSMutablePath()
+//        path.addLatitude(-33.866, longitude:151.195) // Sydney
+//        path.addLatitude(-18.142, longitude:178.431) // Fiji
+//        path.addLatitude(21.291, longitude:-157.821) // Hawaii
+//        path.addLatitude(37.423, longitude:-122.091) // Mountain View
+//        
+//        var polyline = GMSPolyline(path: path)
+//        polyline.strokeColor = UIColor.blueColor()
+//        polyline.strokeWidth = 1.0
+//        polyline.map = mapView
+//        
+//        self.view = mapView
 
 }
 }
