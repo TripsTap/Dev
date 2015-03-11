545001846088bbf0224d249a1b6798e42e977f04
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-03-11 20:30:47 +0700

    add connect server file

---
+++ IOS/TripsTap/TripsTap/Connection.swift
@@ -0,0 +1,13 @@
+//
+//  Connection.swift
+//  TripsTap
+//
+//  Created by Piyawut Kamwiset on 3/11/2558 BE.
+//  Copyright (c) 2558 Piyawut Kamwiset. All rights reserved.
+//
+
+import UIKit
+
+class Connection: NSObject {
+   
+}
