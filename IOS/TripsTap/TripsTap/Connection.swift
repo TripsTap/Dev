16d6191514a171add45dfa53f370454267765b9a
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-03-12 19:06:48 +0700

    create delegate table
    - delegate table
    - add singleton class
    - add some ui on trip-me's page

--- IOS/TripsTap/TripsTap/Connection.swift
+++ IOS/TripsTap/TripsTap/Connection.swift
@@ -9,28 +9,60 @@
 import UIKit
 import Alamofire
 class Connection: NSObject {
-   
-    override init(){
-        println("init conection")
+
+//
+//===============================================================================
+//  singleton
+//===============================================================================
+//
+    class var sharedInstance: Connection {
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+            static var instance: Connection? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = Connection()
+        }
+        return Static.instance!
     }
     
+    override init() {
+        println("init class")
+    }
     
+//
+//===============================================================================
+//  variable
+//===============================================================================
+//
     
     let baseUrl : String = "http://171.98.210.83:8080/TripTap/rest/service/"
     
     
-    func getCategoryTripsMe(location : String ,place : Int   ){
+//
+//===============================================================================
+//  function
+//===============================================================================
+//
+
+    func getCategoryTripsMe(location : String ,place : Int  , completion :((result : NSObject , error : NSError ) ->()) )
+    {
         
         var url = baseUrl + "getCategory"
         
-        Alamofire.request(.GET, url, parameters: ["location": "phuket", "place" : 0]  ).responseJSON { (request, response, data, error) -> Void in
+        Alamofire.request(.GET, url, parameters: ["location": location , "place" : 0]  ).responseJSON { (request, response, data, error) -> Void in
             println(request)
             println(response)
             println(error)
             println("---------------------")
             println(data?.description)
-
+            
+        
+            completion(result: response!, error: error!)
         }
+        
+        
+
     }
     
     func getRuleTripsMe(categories : Int){
--- IOS/TripsTap/TripsTap/TripMeViewController.swift
+++ IOS/TripsTap/TripsTap/TripMeViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class TripMeViewController: UIViewController ,UITableViewDelegate {
+class TripMeViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource{
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    
+//MARK: -
+//MARK: IBOutlet
+//MARK: -
 
     @IBOutlet weak var textLocation: UITextField!
     @IBOutlet weak var teble: UITableView!
     @IBOutlet weak var viewPicker: UIView!
     @IBOutlet weak var viewIndicator: UIView!
-
     
+
+//MARK: -
+//MARK: variable
+//MARK: -
     
     let location : Array<String> = ["Krung Thep Mahanakhon","Chainat" ,"Nakhon Sawan" ,"Nonthaburi" ,"Pathum Thani" ,"Ayutthaya","Lopburi","Samut Songkhram","Samut Prakan","Samut Sakhon","Saraburi","Singburi","Ang Thong","Uthai Thani","Kanchanaburi",    "Nakhon Pathom",    "Prachuap Khiri Khan",    "Phetchaburi",    "Ratchaburi",    "Suphanburi",    "Chanthaburi",    "Chachoengsao",    "Chonburi",    "Trat",    "Nakhon Nayok",    "Prachinburi",    "Rayong",    "Sa Kaeo",    "Kalasin",    "Khon Kaen",    "Chaiyaphum",    "Nakhon Phanom",    "Nakhon Ratchasima",    "Buriram",    "Maha Sarakham",    "Mukdahan",    "Yasothon",    "Roi Et",    "Loei",    "Si Sa Ket",    "Sakon Nakhon",    "Surin",    "Nong Khai",    "Nong Bua Lamphu",    "Amnat Charoen",    "Udon Thani",    "Ubon Ratchathani",    "Krabi",    "Chumphon",    "Trang",    "Nakhon Si Thammarat",    "Narathiwat",    "Pattani",    "Phangnga",    "Phatthalung",    "Phuket",    "Yala",    "Ranong",    "Songkhla",    "Satun",    "Surat Thani",    "Kamphaeng Phet",    "Chiang Rai",    "Chiang Mai",    "Tak",    "Nan",    "Phayao",    "Phichit",    "Phitsanulok",    "Phetchabun",    "Phrae",    "Mae Hong Son",    "Lampang",    "Lamphun",    "Sukhothai",    "Uttaradit"]
     
+    var category : NSMutableArray
+    
+    
+//MARK:-
+//MARK: cycle
+//MARK:-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        category = NSMutableArray()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,11 +50,18 @@ class TripMeViewController: UIViewController ,UITableViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+//MARK: -
+//MARK: button event
+//MARK: -
+    
     @IBAction func clickTestRequest(sender: AnyObject) {
         
         var connect = Connection();
-        connect.getCategoryTripsMe("phuket",place: 0)
+        connect.getCategoryTripsMe("phuket", place: 0) { (result, error) -> () in
+            println(result)
+            println(error)
+        }
     }
     
     @IBAction func clickSelectLocation(sender: AnyObject) {
@@ -49,6 +77,9 @@ class TripMeViewController: UIViewController ,UITableViewDelegate {
     }
 
     
+//MARK:-
+//MARK:  picker function
+//MARK:-
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
@@ -65,6 +96,22 @@ class TripMeViewController: UIViewController ,UITableViewDelegate {
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         textLocation.text = location[row]
+    }
+
+//MARK:-
+//MARK:  picker function
+//MARK:-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return category.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
     
     
