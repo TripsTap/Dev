2ad47d82332be08e3f6ea40c904d4c7c6e68a2d1
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-04-06 14:43:59 +0700

    - add class for manage plan file

---
+++ IOS/TripsTap/TripsTap/PlanFile.swift
@@ -0,0 +1,13 @@
+//
+//  PlanFile.swift
+//  TripsTap
+//
+//  Created by Piyawut Kamwiset on 4/6/2558 BE.
+//  Copyright (c) 2558 Piyawut Kamwiset. All rights reserved.
+//
+
+import UIKit
+
+class PlanFile: NSObject {
+   
+}
