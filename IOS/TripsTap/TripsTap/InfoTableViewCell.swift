c86d13e352af6dfffff0d6e66eae5f17491229b9
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-04-08 20:30:14 +0700

    - add info page cell

---
+++ IOS/TripsTap/TripsTap/InfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InfoTableViewCell.swift
+//  TripsTap
+//
+//  Created by Piyawut Kamwiset on 4/6/2558 BE.
+//  Copyright (c) 2558 Piyawut Kamwiset. All rights reserved.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
