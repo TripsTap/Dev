8cecd2e7ce63cad1e14caa6bef19b4cfb557d160
Piyawut Kamwiset <piyawutkamwiset@Piyawuts-MacBook-Pro.local>
2015-04-20 22:09:36 +0700

    - cell for one place on plan

---
+++ IOS/TripsTap/TripsTap/MainFourTableViewCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainFourTableViewCellTableViewCell.swift
+//  TripsTap
+//
+//  Created by Piyawut Kamwiset on 4/20/2558 BE.
+//  Copyright (c) 2558 Piyawut Kamwiset. All rights reserved.
+//
+
+import UIKit
+
+class MainFourTableViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
